ef5ea718849d723f1bc7b28b2adbd62f60be8dfb
bks1994K <bks1994@mail.ru>
2022-10-07 23:29:20 +0300

    Add project files.

---
+++ 1,17,1/Program.cs
@@ -0,0 +1,25 @@
+﻿using _1_17_1;
+
+int[] array = new int[] { 0,2,3,4,5,6,7,8,9 };
+
+int mins = ArrayHandler.FindMinElementOfArray(array);
+int maxs = ArrayHandler.FindMaxElementOfArray(array);
+int indexMins = ArrayHandler.FindIndexOfMinElementOfArray(array);
+int indexMaxs = ArrayHandler.FindIndexOfMaxElementOfArray(array);
+int summElementsOddIndex = ArrayHandler.SummElementsWithOddIndexOfArray(array);
+int[] mirrow = ArrayHandler.MirroredArray(array);
+ArrayHandler.WriteArrayToConsole(mirrow);
+int oddElements = ArrayHandler.CountOddElementsInArray(array);
+int[] swappedEven = ArrayHandler.SwappedHalfOTheArray(array);
+ArrayHandler.WriteArrayToConsole(swappedEven);
+
+
+Console.WriteLine(mins);
+Console.WriteLine(maxs);
+Console.WriteLine(indexMins);
+Console.WriteLine(indexMaxs);
+Console.WriteLine(summElementsOddIndex);
+Console.WriteLine(mirrow);
+Console.WriteLine(oddElements);
+Console.WriteLine();
+Console.WriteLine(swappedEven);
---
+++ 1,17,1/WorkingWithArray.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _1_17_1
+{
+    public static class ArrayHandler
+
+    //1
+
+    {
+        public static int FindMinElementOfArray(int[] arrays)
+        {
+            int min = arrays[0];
+            for (int i = 1; i < arrays.Length; i++)
+            {
+                if (min > arrays[i])
+                {
+                    min = arrays[i];
+                }
+            }
+            return min;
+        }
+
+
+        //2
+        public static int FindMaxElementOfArray(int[] arrays)
+        {
+            int max = arrays[0];
+            for (int i = 1; i < arrays.Length; i++)
+            {
+                if (max < arrays[i])
+                {
+                    max = arrays[i];
+                }
+            }
+            return max;
+        }
+
+        //3
+
+        public static int FindIndexOfMinElementOfArray(int[] arrays)
+        {
+
+            int min = arrays[0];
+            int n = 0;
+            for (int i = 1; i < arrays.Length; i++)
+            {
+                if (min > arrays[i])
+                {
+                    min = arrays[i];
+                    n = i;
+                }
+            }
+            return n;
+        }
+
+        //4
+
+        public static int FindIndexOfMaxElementOfArray(int[] arrays)
+        {
+
+            int max = arrays[0];
+            int n = 0;
+            for (int i = 1; i < arrays.Length; i++)
+            {
+                if (max < arrays[i])
+                {
+                    max = arrays[i];
+                    n = i;
+                }
+            }
+            return n;
+        }
+
+        //5
+
+        public static int SummElementsWithOddIndexOfArray(int[] arrays)
+        {
+
+            int n = 0;
+            int summ = 0;
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                n = i;
+                int tmp = n % 2;
+                if (tmp != 0)
+                {
+                    summ = summ + arrays[i];
+                }
+            }
+            return summ;
+        }
+
+        //6
+
+        public static int[] MirroredArray(int[] arrays)
+        {
+            int[] mirrow = new int[arrays.Length];
+            int k = -1;
+            for (int i = arrays.Length - 1; i >= 0; i--)
+            {
+                k = k + 1;
+                mirrow[k] = arrays[i];
+            }
+            return mirrow;
+        }
+
+        //6.1
+
+        public static void WriteArrayToConsole(int[] arrays)
+        {
+            
+            for (int i = 0; i < arrays.Length; i++)
+
+            {
+                Console.Write(arrays[i]);
+            }
+            Console.WriteLine();
+        }
+
+        //7
+
+        public static int CountOddElementsInArray(int[] arrays)
+        {
+            int count = 0;
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                int tmp = arrays[i] % 2;
+                if (tmp != 0)
+                {
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+
+        //8
+
+        public static int[] SwappedHalfOTheArray(int[] arrays)
+        {
+            int[] swapped = new int[arrays.Length];
+
+            int count = 0;
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                count = count + 1;
+            }
+
+            int leftEven = count / 2;
+            int index = count - leftEven;
+            for (int j = 0; j < leftEven; j++)
+            {
+                swapped[index] = arrays[j];
+                index = index + 1;
+            }
+            index = 0;
+            for (int k = leftEven; k <= count - 1; k++)
+            {
+                swapped[index] = arrays[k];
+                index = index + 1;
+            }
+
+            return swapped;
+        }
+    }
+}
